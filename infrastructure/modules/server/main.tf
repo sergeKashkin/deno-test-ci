b1ba95b4626b5749b38972463b5e4b8c636093fd
Sergei <sergmaloi@gmail.com>
2024-11-06 22:34:18 +0200

    TF correct; Docker-compose correct;

--- infrastructure/modules/server/main.tf
+++ infrastructure/modules/server/main.tf
@@ -15,10 +15,14 @@ resource "aws_instance" "app_server" {
     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
     sudo apt update
     sudo apt-get install docker-ce -y
+    sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
     sudo systemctl start docker
     sudo systemctl enable docker
     sudo groupadd docker
     sudo usermod -aG docker ubuntu
+
+    sudo apt install awscli -y
   EOF
   user_data_replace_on_change = true
 
